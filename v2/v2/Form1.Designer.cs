798cb2b4760ca97448fcf0268839daf8b54125ed
Henock <henockmamo54@gmail.com>
2022-01-03 17:46:50 -0600

    peptide grid on click listner added

--- v2/v2/Form1.Designer.cs
+++ v2/v2/Form1.Designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
@@ -42,33 +42,33 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(594, 56);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(412, 56);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series2.Legend = "Legend1";
-            series2.Name = "Series2";
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series3.Legend = "Legend1";
-            series3.Name = "Series3";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series4.Legend = "Legend1";
-            series4.Name = "Series4";
-            this.chart1.Series.Add(series1);
-            this.chart1.Series.Add(series2);
-            this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(446, 387);
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.Name = "Series2";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Legend = "Legend1";
+            series7.Name = "Series3";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Legend = "Legend1";
+            series8.Name = "Series4";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Size = new System.Drawing.Size(628, 268);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
@@ -82,6 +82,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(300, 387);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // Form1
             // 
--- v2/v2/Form1.cs
+++ v2/v2/Form1.cs
@@ -107,7 +107,7 @@ namespace v2
             dt.Rows.Add("sample Peptide 7");
             dt.Rows.Add("sample Peptide 8");
             dt.Rows.Add("sample Peptide 9");
-            
+
             //Bind data to grid view
             dataGridView1.DataSource = dt;
 
@@ -115,5 +115,109 @@ namespace v2
             // hide row selector
             dataGridView1.RowHeadersVisible = false;
         }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            int rowIndex = e.RowIndex;
+            if (rowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
+                var temp = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+
+                double[] k_val = { 0.23, 0.25, 0.31, 0.35 };
+                int[] t_val = { 0, 1, 3, 5, 7, 21 };
+
+                t_val = t_val.Select(x => x * (1+rowIndex)).ToArray();
+                k_val = k_val.Select(x => x + (x/(rowIndex+1))).ToArray();
+
+                loadcharts(k_val, t_val);
+
+                MessageBox.Show(temp);
+            }
+
+
+        }
+
+        public void loadcharts(double[] k_val, int[] t_val)
+        {
+
+            //double[] k_val = { 0.23, 0.25, 0.31, 0.35 };
+            //int[] t_val = { 0, 1, 3, 5, 7, 21 };
+
+            //clear the chart 
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+                chart1.Update();
+
+                
+            }
+
+
+            //# compute chart values 
+            List<List<double>> y_vals = new List<List<double>>();
+
+            foreach (double k in k_val)
+            {
+                List<double> temp_y = new List<double>();
+                foreach (int t in t_val)
+                {
+                    var val = 1 - Math.Pow(Math.E, (-k * t));
+                    temp_y.Add(val);
+                }
+                y_vals.Add(temp_y);
+
+            }
+
+
+            for (int i = 0; i < k_val.Length; i++)
+            {
+                for (int j = 0; j < t_val.Length; j++)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            chart1.Series["Series1"].Points.AddXY(t_val[j], y_vals[i][j]);
+                            break;
+                        case 1:
+                            chart1.Series["Series2"].Points.AddXY(t_val[j], y_vals[i][j]);
+                            break;
+                        case 2:
+                            chart1.Series["Series3"].Points.AddXY(t_val[j], y_vals[i][j]);
+                            break;
+                        case 3:
+                            chart1.Series["Series4"].Points.AddXY(t_val[j], y_vals[i][j]);
+                            break;
+                    }
+
+                }
+            }
+
+            // add lables 
+            //chart1.Series["Series1"].Label = " k = " + k_val[0];
+            //chart1.Series["Series2"].Label = " k = " + k_val[1];
+            //chart1.Series["Series3"].Label = " k = " + k_val[2];
+            //chart1.Series["Series4"].Label = " k = " + k_val[3];
+
+            // remove grid lines
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisX.MinorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MinorGrid.Enabled = false;
+
+            // chart add legend
+            chart1.Series["Series1"].LegendText = " k = " + k_val[0];
+            chart1.Series["Series2"].LegendText = " k = " + k_val[1];
+            chart1.Series["Series3"].LegendText = " k = " + k_val[2];
+            chart1.Series["Series4"].LegendText = " k = " + k_val[3];
+
+            // chart labels added 
+            chart1.ChartAreas[0].AxisX.Title = "Time (days)";
+            chart1.ChartAreas[0].AxisY.Title = "Lys0/LysTotal";
+
+            chart1.Update();
+        }
+
     }
 }
