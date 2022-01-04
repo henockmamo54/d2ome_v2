da838d3f36f196e9cb01233af985cf658a2dbcc8
Henock <henockmamo54@gmail.com>
2022-01-04 12:34:45 -0600

    graph export added

--- v2/v2/Form1.cs
+++ v2/v2/Form1.cs
@@ -219,5 +219,38 @@ namespace v2
             chart1.Update();
         }
 
+        public bool exportchart() {
+            try
+            {
+                using (Bitmap im = new Bitmap(chart1.Width, chart1.Height))
+                {
+                    chart1.DrawToBitmap(im, new Rectangle(0, 0, chart1.Width, chart1.Height));
+                    //using (Graphics gr = Graphics.FromImage(im))
+                    //{
+                    //    gr.DrawString("Test",
+                    //        new Font(FontFamily.GenericSerif, 10, FontStyle.Bold),
+                    //        new SolidBrush(Color.Red), new PointF(10, 10));
+                    //}
+                    im.Save("F:\\workplace\\d2ome_v2\\test.jpeg");
+
+
+                }
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (exportchart())
+            {
+                MessageBox.Show("chart generated");
+            }
+            else {
+                MessageBox.Show("! file not genrated");
+            }
+        }
     }
 }
