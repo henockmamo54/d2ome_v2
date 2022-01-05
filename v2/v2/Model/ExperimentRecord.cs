630953ee8a634327ae18180fe632b7efd00bfeee
Henock <henockmamo54@gmail.com>
2022-01-04 18:53:17 -0600

    peptide and Experiment record model added

---
+++ v2/v2/Model/ExperimentRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v2.Model
+{
+    public class ExperimentRecord
+    {
+        public string experimentName { get; set; }
+        public string PeptideSeq { get; set; }
+        public double SpecMass { get; set; }
+        public double? IonScore { get; set; }
+        public double? Expectn { get; set; }
+        public double? Error { get; set; }
+        public double? Scan { get; set; }
+        public double? I0 { get; set; }
+        public double? I1 { get; set; }
+        public double? I2 { get; set; }
+        public double? I3 { get; set; }
+        public double? I4 { get; set; }
+        public double? I5 { get; set; }
+        public double? Start_Elution { get; set; }
+        public double? End_Elution { get; set; }
+        public double? I0_Peak_Width { get; set; }
+        public double? Total_Labeling { get; set; }
+        public double? Net_Labeling { get; set; }
+
+    }
+}
---
+++ v2/v2/Model/Peptide.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v2.Model
+{
+    public class Peptide
+    {
+        public string PeptideSeq { get; set; }
+        public bool? UniqueToProtein { get; set; }
+        public double? Exchangeable_Hydrogens { get; set; }
+        public double? Charge { get; set; }
+        public double? SeqMass { get; set; }
+        public double? M0 { get; set; }
+        public double? M1 { get; set; }
+        public double? M2 { get; set; }
+        public double? M3 { get; set; }
+        public double? M4 { get; set; }
+        public double? M5 { get; set; }
+        public double? Total_Labeling { get; set; }
+    }
+}
