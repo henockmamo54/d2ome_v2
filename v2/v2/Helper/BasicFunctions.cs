7bdd1f99bb59549696c0ab6710c25e9757a97d17
Henock <henockmamo54@gmail.com>
2022-01-06 11:38:42 -0600

    ria model added

---
+++ v2/v2/Helper/BasicFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v2.Helper
+{
+    public class BasicFunctions
+    {
+        public static double computeRIA(double i0, double i1, double i2, double i3, double i4, double i5)
+        {
+            return i0 / (i0 + i1 + i2 + i3 + i4 + i5);
+        }
+    }
+}
---
+++ v2/v2/Model/RIA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v2.Model
+{
+    public class RIA
+    {
+        public string experimentName { get; set; }
+        public List<string> experimentNames { get; set; }
+        public string peptideSeq { get; set; }
+        public int time { get; set; }
+        public double? I0 { get; set; }
+        public double? RIA_value { get; set; }
+    }
+}
