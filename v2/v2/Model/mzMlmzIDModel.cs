6e019fd5e1de00c6b7de282f773d0d53b22d23b5
Henock <henockmamo54@gmail.com>
2022-01-18 14:00:39 -0600

    mzmid data model added

---
+++ v2/v2/Model/mzMlmzIDModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v2.Model
+{
+    public class mzMlmzIDModel
+    {
+        public string mzML { get; set; }
+        public string mzID { get; set; }
+        public string T { get; set; }
+        public string BWE { get; set; }
+    }
+}
