a00fd0f992d9c5f9c34464ff4016e0d527e0d0c5
Henock <henockmamo54@gmail.com>
2022-03-08 17:37:35 -0800

    get best path option added

---
+++ v2/Get/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+
+        }
+
+        public static void GetBestPath(float[,] inputdata)
+        {
+            int rows = inputdata.GetLength(0);
+            int columns = inputdata.GetLength(1);
+            List<List<float>> best_path = new List<List<float>>();
+            for (int i = 0; i < rows; i++)
+            {
+                var temp_path_i = new List<float>();
+                //temp_path_i.Add(inputdata[i, 0]);
+                temp_path_i.Add(0);
+
+                for (int j = 1; j < columns; j++)
+                {
+                    // check montonosity. if the next data point is greater than the current value, choose it as the next path
+                    if (inputdata[i, j] > inputdata[i, j - 1])
+                    {
+                        temp_path_i.Add(inputdata[i, j]);
+                    }
+                    else
+                    {
+                        var min_value = 1E6;
+                        float next_value = (float)1E6;
+                        for (int k = 1; k < rows; k++)
+                        {
+                            if (inputdata[k, j] > inputdata[i, j - 1] && (inputdata[k, j] - inputdata[i, j - 1] < min_value))
+                            {
+                                min_value = inputdata[k, j] - inputdata[i, j - 1];
+                                next_value = inputdata[k, j];
+                            }
+                            else if (inputdata[k, j] < inputdata[i, j - 1] && min_value < 0 && (inputdata[k, j] - inputdata[i, j - 1] > min_value))
+                            {
+                                min_value = inputdata[k, j] - inputdata[i, j - 1];
+                                next_value = inputdata[k, j];
+                            }
+                        }
+                        if (min_value != 1E6) temp_path_i.Add(next_value);
+                        else temp_path_i.Add(temp_path_i[temp_path_i.Count() - 1]);
+
+                    }
+                }
+                best_path.Add(temp_path_i);
+            }
+        }
+    }
+}
--- v2/v2/Helper/BasicFunctions.cs
+++ v2/v2/Helper/BasicFunctions.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace v2.Helper
 {
-    public class BasicFunctions
+    public static class BasicFunctions
     {
         public static double computeRIA(double i0, double i1, double i2, double i3, double i4, double i5)
         {
@@ -22,6 +22,82 @@ namespace v2.Helper
             else tempval = String.Format("{0:0.######}", n);
 
             return tempval;
+        }
+        public static List<List<double>> GetBestPath(float[,] inputdata)
+        {
+            int rows = inputdata.GetLength(0);
+            int columns = inputdata.GetLength(1);
+            List<List<double>> best_path = new List<List<double>>();
+            for (int i = 0; i < rows; i++)
+            {
+
+
+                var temp_path_i = new List<double>();
+                temp_path_i.Add(inputdata[i, 0]);
+                //temp_path_i.Add(0);
+
+                for (int j = 1; j < columns; j++)
+                {
+                    if (inputdata[i, j] == 0)
+                    {
+                        temp_path_i.Add(double.NaN);
+                        continue;
+                    }
+
+                    var previous_dataPoint = temp_path_i[temp_path_i.Count - 1];
+                    int counter = 1;
+                    while (double.IsNaN(previous_dataPoint))
+                    {
+                        previous_dataPoint = temp_path_i[temp_path_i.Count - 1 - counter];
+                        counter++;
+                    }
+
+
+
+                    // check montonosity. if the next data point is greater than the current value, choose it as the next path
+                    if (inputdata[i, j] > previous_dataPoint)
+                    {
+                        temp_path_i.Add(inputdata[i, j]);
+                    }
+                    else
+                    {
+                        var min_value = 1E6;
+                        float next_value = (float)1E6;
+
+                        for (int k = 0; k < rows; k++)
+                        {
+                            if (inputdata[k, j] >= previous_dataPoint && ((inputdata[k, j] - previous_dataPoint) <= min_value))
+                            {
+                                min_value = inputdata[k, j] - previous_dataPoint;
+                                next_value = inputdata[k, j];
+                            }
+
+                        }
+
+                        if (min_value == 1E6)
+                        {
+                            for (int k = 0; k < rows; k++)
+                            {
+                                if (Math.Abs(inputdata[k, j] - previous_dataPoint) <= min_value)
+                                {
+                                    min_value = Math.Abs(inputdata[k, j] - previous_dataPoint);
+                                    next_value = inputdata[k, j];
+                                }
+                            }
+
+                        }
+
+                        if (min_value == 1E6)
+                            temp_path_i.Add(double.NaN);
+                        else
+                            temp_path_i.Add(next_value);
+
+
+                    }
+                }
+                best_path.Add(temp_path_i);
+            }
+            return best_path;
         }
     }
 }
