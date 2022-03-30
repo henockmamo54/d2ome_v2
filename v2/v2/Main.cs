d15815e3b7ceded2be5b0fdecd8a96830d3c6333
Henock <henockmamo54@gmail.com>
2022-03-30 18:17:45 -0500

    chart modified

--- v2/v2/Main.cs
+++ v2/v2/Main.cs
@@ -1289,40 +1289,40 @@ elutionwindow, peptideconsistency, rate_constant_choice, protienscore, protienco
 
                 s1.ChartType = SeriesChartType.FastPoint;
                 s1.Color = Color.Red;
-                s1.BorderWidth = 2;
+                s1.MarkerSize = 4;
                 chart_peptide.Series.Add(s1);
 
                 // new computation plot
 
                 if (chart_peptide.Series.FindByName("A1/A0") != null)
                     chart_peptide.Series.Remove(chart_peptide.Series.FindByName("A1/A0"));
-                if (chart_peptide.Series.FindByName("A2/A0 + A1/A0") != null)
-                    chart_peptide.Series.Remove(chart_peptide.Series.FindByName("A2/A0 + A1/A0"));
-                if (chart_peptide.Series.FindByName("pX(t) > 0.05") != null)
-                    chart_peptide.Series.Remove(chart_peptide.Series.FindByName("pX(t) > 0.05"));
+                if (chart_peptide.Series.FindByName("A2/A0") != null)
+                    chart_peptide.Series.Remove(chart_peptide.Series.FindByName("A2/A0"));
+                if (chart_peptide.Series.FindByName("A2/A1") != null)
+                    chart_peptide.Series.Remove(chart_peptide.Series.FindByName("A2/A1"));
 
                 Series s_A1 = new Series();
                 s_A1.Name = "A1/A0";
                 s_A1.Points.DataBindXY(chart_data.Select(x => x.Time).ToArray(), chart_data.Select(x => x.I0_t_fromA1).ToArray());
                 s_A1.ChartType = SeriesChartType.FastPoint;
                 s_A1.Color = Color.Green;
-                s_A1.BorderWidth = 2;
+                s_A1.MarkerSize = 7;
                 chart_peptide.Series.Add(s_A1);
 
                 Series s_A2 = new Series();
-                s_A2.Name = "A2/A0 + A1/A0";
+                s_A2.Name = "A2/A0";
                 s_A2.Points.DataBindXY(chart_data.Select(x => x.Time).ToArray(), chart_data.Select(x => x.I0_t_fromA1A2).ToArray());
                 s_A2.ChartType = SeriesChartType.FastPoint;
-                s_A2.Color = Color.BlueViolet; 
-                s_A2.BorderWidth = 2;
+                s_A2.Color = Color.BlueViolet;
+                s_A2.MarkerSize = 7;
                 chart_peptide.Series.Add(s_A2);
 
                 Series s_pxt = new Series();
-                s_pxt.Name = "pX(t) > 0.05";
+                s_pxt.Name = "A2/A1";
                 s_pxt.Points.DataBindXY(chart_data.Select(x => x.Time).ToArray(), chart_data.Select(x => x.pX_greaterthanThreshold).ToArray());
                 s_pxt.ChartType = SeriesChartType.FastPoint;
                 s_pxt.Color = Color.OrangeRed;
-                s_pxt.BorderWidth = 2;
+                s_pxt.MarkerSize = 7;
                 chart_peptide.Series.Add(s_pxt);
 
 
