3d8896a33bb86de045c277311510526bba013196
Henock <henockmamo54@gmail.com>
2022-04-26 13:12:17 -0500

    labeling unit modified

--- v2/v2/Main.cs
+++ v2/v2/Main.cs
@@ -349,12 +349,12 @@ namespace v2
 
                 // labeling_time_unit
                 //string labeling_time_unit = comboBox_labelingtimeunit.Text;
-                double labeling_time_unit = 0;
+                string labeling_time_unit = "Days";
 
                 if (comboBox_labelingtimeunit.Text == "Days")
-                    labeling_time_unit = 1;
+                    labeling_time_unit = "Days";
                 else if (comboBox_labelingtimeunit.Text == "Hours")
-                    labeling_time_unit = 1 / 24.0;
+                    labeling_time_unit = "Hours";
                 else
                 {
                     MessageBox.Show("Invalid labeling time unit. " + comboBox_labelingtimeunit +
@@ -379,7 +379,7 @@ elutiontimewindow   =   {3}  // time window  (mins) to search for elution peak. 
 protein_consistency = {7}  // minimum number of experiments for protein consistency
 peptide_consistency = {4}   //mininum number of experiments for a peptide consistency
 NParam_RateConst_Fit = {5}	// The model for fitting rate constant. Values are 1, and 2
-Labeling_time_unit = {9}  // 1 = Days, 0.0416666666666667 = Hours (1/24)
+Labeling_time_unit = {9}  // Labeling time unit  Days or Hours  
 ", massaccuracy, MS1_Type, peptidescore,
 elutionwindow, peptideconsistency, rate_constant_choice, protienscore, protienconsistency, peptide_expectation, labeling_time_unit);
 
